5b98391764f03caa1fca3c49c7b59a2c03c268c6
sebaactis <sebaactis@gmail.com>
2024-02-22 19:54:33 -0300

    Modal Inicial para crear servidor (react-hook-form + schacn)

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,3 +1,4 @@
+import InitialModal from "@/components/modals/initial-modal"
 import { db } from "@/lib/db"
 import { initialProfile } from "@/lib/initial-profile"
 import { redirect } from "next/navigation"
@@ -20,6 +21,6 @@ export default async function SetupPage() {
     }
 
     return (
-        <div>Create a Server</div>
+        <InitialModal />
     )
 }
