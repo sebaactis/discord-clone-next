2d4b97bd7b6f0da84d2677a37aafb307aca8ea1a
sebaactis <sebaactis@gmail.com>
2024-02-26 14:27:07 -0300

    create server modal + modalProvider + useModal con zustand

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from "@/components/providers/theme-provider";
-import { cn } from "@/lib/utils";
+import { ModalProvider } from "@/components/providers/modal-provider";
+
 
 
 const font = Open_Sans({ subsets: ["latin"] });
@@ -22,7 +25,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body className={cn(font.className,
-        "bg-white dark:bg-[#313338]"
+          "bg-white dark:bg-[#313338]"
         )}>
           <ThemeProvider
             attribute="class"
@@ -30,6 +33,7 @@ export default function RootLayout({
             enableSystem
             storageKey="discord-theme"
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
