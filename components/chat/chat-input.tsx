cc62286846e8d4b003fa020398b1fcdcdc4ee15b
sebaactis <sebaactis@gmail.com>
2024-03-11 15:28:34 -0300

    upload attachments at messages

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -9,6 +9,7 @@ import { Plus, Smile } from "lucide-react"
 
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { useModal } from "@/hooks/use-modal-store"
 
 interface ChatInputProps {
     apiUrl: string
@@ -27,6 +28,8 @@ export const ChatInput = ({
     name,
     type
 }: ChatInputProps) => {
+
+    const { onOpen } = useModal();
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -57,7 +60,7 @@ export const ChatInput = ({
                     <FormItem>
                         <FormControl>
                             <div className="relative p-4 pb-6">
-                                <button type="button" onClick={() => { }} className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center">
+                                <button type="button" onClick={() => onOpen("messageFile", { apiUrl, query })} className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center">
                                     <Plus className="text-white dark:text-[#313338]" />
                                 </button>
                                 <Input placeholder={`Message ${type === "conversation" ? name : "#" + name}`} disabled={isLoading} className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200" {...field} />
