9ea964e7ae489582b1d8062d739be5d523cfd60f
sebaactis <sebaactis@gmail.com>
2024-03-13 12:59:34 -0300

    delete message

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -15,6 +15,7 @@ import { cn } from "@/lib/utils"
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { useModal } from "@/hooks/use-modal-store"
 
 interface ChatItemProps {
     id: string
@@ -44,7 +45,7 @@ const formSchema = z.object({
 export const ChatItem = ({ id, content, member, timestamp, fileUrl, deleted, currentMember, isUpdated, socketQuery, socketUrl }: ChatItemProps) => {
 
     const [isEditing, setIsEditing] = useState(false)
-    const [isDeleting, setIsDeleting] = useState(false)
+    const { onOpen } = useModal();
 
     useEffect(() => {
         const handleKeyDown = (event: any) => {
@@ -181,7 +182,12 @@ export const ChatItem = ({ id, content, member, timestamp, fileUrl, deleted, cur
                         </ActionToolTip>
                     )}
                     <ActionToolTip label="Delete">
-                        <Trash className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition" />
+                        <Trash
+                            onClick={() => onOpen("deleteMessage", {
+                                apiUrl: `${socketUrl}/${id}`,
+                                query: socketQuery
+                            })}
+                            className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition" />
                     </ActionToolTip>
                 </div>
             )}
