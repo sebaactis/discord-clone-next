e9f7bf68a12c6eb27cf86096201dc400759f08b7
Sebastian Actis <Sebastian.Actis@globalprocessing.com.ar>
2024-03-27 15:16:40 -0300

    comments de provider socket y query provider

--- components/providers/query-provider.tsx
+++ components/providers/query-provider.tsx
@@ -3,6 +3,9 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { useState } from "react"
 
+// Creamos el provider para el queryClient de react-query.
+// Es nuestro cliente para poder usar los hooks de react-query.
+
 export const QueryProvider = ({ children }: { children: React.ReactNode }) => {
 
     const [queryClient] = useState(() => new QueryClient())
--- components/providers/socket-provider.tsx
+++ components/providers/socket-provider.tsx
@@ -3,23 +3,45 @@
 import { createContext, useContext, useEffect, useState } from "react"
 import { io as ClientIO } from "socket.io-client"
 
+// El siguiente provider es para crear la conexion principal del socket y envolver nuestra aplicacion dentro de el.
+
+// Vamos a crear un tipo para el socket y para el isConnected
+
 type SocketContextType = {
     socket: any | null
     isConnected: boolean
 }
+
+// Vamos a crear un context de tipo SocketContextType, para tener el socket en toda la app.
 
 const SocketContext = createContext<SocketContextType>({
     socket: null,
     isConnected: false
 })
 
+// Creamos un mini hook que retorne el uso del contexto directamente.
+
 export const useSocket = () => {
     return useContext(SocketContext)
 }
 
+// Creamos el provider que envolvera la aplicacion con el socket.
+
 export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
+
+
+    // Creamos un estado para guardar el socket (la instancia), y uno para setear si esta conectado o no al socket.
+
     const [socket, setSocket] = useState(null)
     const [isConnected, setIsConnected] = useState(false)
+
+    // Creamos un efecto para crear la instancia del socket del lado del cliente, y la enviamos hacia la ruta informada.
+
+    // Creamos dos eventos de escucha, cuando nos conectamos ("connect") y cuando nos desconectamos ("disconnect").
+
+    // Y seteamos la instancia del socket en nuestro estado socket.
+
+    // Por ultimo, desconectamos la instancia para no crear instancias de forma masiva.
 
     useEffect(() => {
         const socketInstance = new (ClientIO as any)(process.env.NEXT_PUBLIC_SITE_URL!, {
